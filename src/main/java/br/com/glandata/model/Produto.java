7f78de8916f462ce08fdccd252d38bfd5dae8e47
Ailton Occhi <ailton.occhi@hotmail.com>
2022-02-13 16:56:21 -0300

    Commit 03 - Anotações Index e Unique

--- src/main/java/br/com/glandata/model/Produto.java
+++ src/main/java/br/com/glandata/model/Produto.java
@@ -6,13 +6,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "produtos")
+@Table(
+		name = "produtos", 
+		indexes = { @Index(name = "id_produto", columnList = "id")}, 
+		uniqueConstraints = {@UniqueConstraint(name = "nome_unique", columnNames = "nome") }
+		)
 public class Produto {
 
 	@Getter @Setter
